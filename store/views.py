2e62bd048e664bf365f52e9b95ffb01fb3893fb0
vaibhav519 <vaibhav3raj@gmail.com>
2022-03-18 01:23:08 +0530

    getting current user's profile.

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
+import re
 from select import select
 from django.db.models.aggregates import Count
 from django.http import QueryDict
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.response import Response
@@ -80,4 +82,17 @@ class CartItemViewSet(ModelViewSet):
 
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer  
+    serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=['GET, PUT'])
+    def me(self, request):
+        (customer, created) = Customer.objects.get(user_id= request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.save()
+            return Response(serializer.data)
+
+ 
