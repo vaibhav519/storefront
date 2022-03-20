ce328d4ea9d01541b96d9eb4e00a90300e9f87ea
vaibhav519 <vaibhav3raj@gmail.com>
2022-03-20 20:39:18 +0530

    Applying Model permissions.

--- store/permissions.py
+++ store/permissions.py
@@ -7,3 +7,5 @@ class IsAdminOrReadOnly(BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
+
+    
